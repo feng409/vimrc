5eb1f123a04c321cc0ab958e93881af92b3078a2
feng409 <eoyohe@gmail.com>
2019-09-25 17:01:04 +0800

    [IMP] add the install script

---
+++ install.sh
@@ -0,0 +1,49 @@
+#/usr/bin/env bash
+# install common dependency
+echo -n "choose python version command to compile YouCompleteMe (python2|python3): "
+read python
+
+source /etc/os-release
+
+case $ID in
+debian|ubuntu)
+	sudo apt-get install -y \
+		git \
+		vim \
+		ctags \
+		cmake \
+		ack \
+		build-essential \
+		python3-dev
+	;;
+centos|amaz|fedora|rhel)
+	sudo yum install -y \
+		git \
+		vim \
+		ctags \
+		cmake \
+		ack 
+	;;
+*)
+	;;
+esac
+
+# clone thie repo
+git clone --depth 1 https://github.com/feng409/vimrc.git $HOME/.vim
+
+# install plugin by vundle
+git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git Vundle.vim 
+mv Vundle.vim $HOME/.vim/bundle/Vundle.vim 
+
+# alter git clone just for depth 1
+sed -i "s/git clone/git clone --depth 1/" $HOME/.vim/bundle/Vundle.vim/autoload/vundle/installer.vim 
+
+# source vim
+mv $HOME/.vimrc $HOME/.vimrc.bak
+ln -s $HOME/.vim/vimrc $HOME/.vimrc 
+
+vim +PluginInstall +qall
+
+# install YouCompleteMe
+cd $HOME/.vim/bundle/YouCompleteMe \
+	&& $python install.py --clang-completer
--- vimrc
+++ vimrc
@@ -145,7 +145,6 @@ autocmd FileType python setlocal completeopt-=preview
 let g:jedi#goto_command = "<leader>d"
 let g:jedi#goto_assignments_command = "<leader>g"
 let g:jedi#goto_definitions_command = ""
-"let g:jedi#documentation_command = "K"
 let g:jedi#usages_command = "<leader>n"
 let g:jedi#completions_command = "<C-Q>"
 let g:jedi#rename_command = "<leader>r"
