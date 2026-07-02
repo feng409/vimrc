3bbd5addea94a869f6c5810db0a197c31a07ce63
chemf <eoyohe@gmail.com>
2026-07-02 20:05:26 +0800

    ⬆️ Bump LazyVim to 16.* and update lockfile

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", version = "15.*", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", version = "16.*", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
   },
